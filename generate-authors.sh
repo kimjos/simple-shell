693feb81c6141f54704318a1d74cd30149e1d846
kimjos <josephkimani500@gmail.com>
2024-01-15 11:50:10 -0800

    authors file

--- generate-authors.sh
+++ generate-authors.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/../simple-shell"
 
 # see also ".mailmap" for how email addresses and names are deduplicated
 
@@ -13,3 +13,4 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
 	echo
 	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
 } > AUTHORS
+
